c5070733bde65eab4779232d75a789b8ce323d65
BahyAllaAllam <bahy.alam@yahoo.com>
2024-02-23 06:41:59 -0800

    2-sort.rb

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -17,13 +17,15 @@ ARGV.each do |arg|
     i = 0
     l = result.size
     while !is_inserted && i < l do
-        if i == l || result[i] >= i_arg
+        if result[i] <= i_arg
+            i += 1
+        else
             result.insert(i, i_arg)
             is_inserted = true
-        else
-            i += 1
+            break
         end
     end
+    result << i_arg if !is_inserted
 end
 
 puts result
